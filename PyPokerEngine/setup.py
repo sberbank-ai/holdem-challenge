ec96f1e25b5bc18c8dfaa7490a1e1496deb7c579
Peter Romov <peter@romov.ru>
2017-09-16 13:20:25 +0300

    setup.py updated

--- PyPokerEngine/setup.py
+++ PyPokerEngine/setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'PyPokerEngine',
-    version = '1.0.1',
-    author = 'ishikota',
-    author_email = 'ishikota086@gmail.com',
-    description = 'Poker engine for poker AI development in Python ',
-    license = 'MIT',
-    keywords = 'python poker emgine ai',
-    url = 'https://github.com/ishikota/PyPokerEngine',
-    packages = [pkg for pkg in find_packages() if pkg != "tests"],
+    name='sberbank-pypokerengine',
+    version='1.1.1',
+    author='Peter Romov, ishikota',
+    author_email='romovpa@gmail.com, ishikota086@gmail.com',
+    description='Poker engine for poker AI development adapted for Sberbank Holdem Challenge 2017',
+    license='MIT',
+    keywords='python, poker engine, ai, sberbank, holdem challenge',
+    url='http://github.com/sberbank-ai/holdem-challenge',
+    packages=find_packages(include=('pypokerengine', 'pypokerengine.*',)),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    )
-
+)
